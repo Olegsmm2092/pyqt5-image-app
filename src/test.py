964ecbc63b0d874eefd0de03f643c9fe717c02f9
Olegsmm2092 <radoveev@gmail.com>
2024-09-14 01:28:07 +0800

    Add functionality for depth-limited file listing and display file names only

--- src/test.py
+++ src/test.py
@@ -9,7 +9,6 @@ root = QWidget()
 root.setWindowTitle("Lens")
 root.resize(901, 701)
 
-
 # All app widgets/objects
 btn_folder = QPushButton("Folder")
 file_list = QListWidget()
@@ -23,7 +22,6 @@ gray = QPushButton("B/W")
 saturation = QPushButton("Adj Saturation")
 contrast = QPushButton("Adj Contrast")
 blur = QPushButton("Apply Blur")
-
 
 # Dropdown box
 filter_box = QComboBox()
@@ -64,46 +62,49 @@ master_layout.addLayout(right_layout, 80)
 
 root.setLayout(master_layout)
 
-
 # All App Functionality
 working_directory = ""
-# allowed_extentions = '.jpg .jpeg .png .svg'.split() # debug > solution > local inside func; to not to be overwriting
 
-# All App Functionality
-working_directory = ""
+
+# Get All Files Recursively
+def getAllFiles(output_dir, max_depth=2):
+    """get files inside deeper folders with adaptive max depth"""
+    all_files = []
+    base_depth = output_dir.rstrip(os.path.sep).count(os.path.sep)
+    for root, dirs, files in os.walk(output_dir):
+        cur_depth = root.count(os.path.sep) - base_depth
+        if cur_depth < max_depth:
+            for file in files:
+                all_files.append(os.path.join(root, file))
+        dirs[:] = [i for i in dirs if cur_depth + 1 < max_depth]
+    return all_files
+
 
 # Filter files by allowed extensions
 def filter_files_by_extension(files, extensions):
+    """Util to func getWrokDirectory"""
     return [file for file in files if any(file.lower().endswith(ext) for ext in extensions)]
+
 
 # Choose current work directory
 def getWorkDirectory():
     global working_directory
     working_directory = QFileDialog.getExistingDirectory(
         None, "Select Directory")
-    # only local; cant be by args cuz will be `overwriting`
-    allowed_extensions = ['.jpg', '.jpeg', '.png', '.svg']
-    all_files = os.listdir(working_directory)
+    allowed_extensions = ['.jpg', '.jpeg', '.png', '.svg'] # only local; cant be by args cuz will be `overwriting`
+    # all_files = os.listdir(working_directory) # modify using ds googleColab; for deep folder
+    all_files = getAllFiles(working_directory)
     filtered_files = filter_files_by_extension(all_files, allowed_extensions)
     file_list.clear()  # reset
     # Add files to QListWidget
     for file in filtered_files:
-        file_list.addItem(file)
+        file_list.addItem(os.path.basename(file)) # extract only filename;
+
+
+from core.utils import ImageEditor
 
 
 btn_folder.clicked.connect(getWorkDirectory)
-
-
-
-
-
-
-
-
-
-
-
-
 
 root.show()
 app.exec_()
